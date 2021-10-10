5b6edd0f8b179bb78fe012a155e4964d95c84aae
Matthew Casperson <matthewcasperson@gmail.com>
2021-10-10 15:42:34 +1000

    Initial commit

---
+++ src/main/java/com/matthewcasperson/onenote/OnenoteApplication.java
@@ -0,0 +1,13 @@
+package com.matthewcasperson.onenote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnenoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnenoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matthewcasperson/onenote/OnenoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matthewcasperson.onenote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnenoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
