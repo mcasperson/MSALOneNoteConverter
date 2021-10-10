6364c37b6900c6a1693c15eb7e667066320aab30
Matthew Casperson <matthewcasperson@gmail.com>
2021-10-10 06:53:47 +0000

    working on obo auth provider

---
+++ src/main/java/com/matthewcasperson/onenote/authproviders/OboAuthenticationProvider.java
@@ -0,0 +1,45 @@
+package com.matthewcasperson.onenote.authproviders;
+
+import java.net.URL;
+import java.util.concurrent.CompletableFuture;
+
+import com.microsoft.graph.authentication.BaseAuthenticationProvider;
+
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import reactor.core.publisher.Mono;
+
+/**
+ * https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-on-behalf-of-flow#first-case-access-token-request-with-a-shared-secret
+ */
+public class OboAuthenticationProvider extends BaseAuthenticationProvider {
+
+    private final String accessToken;
+    private final String tenant;
+    private final String clientId;
+    private final String clientSecret;
+    private final String scope;
+
+    public OboAuthenticationProvider(final String accessToken, final String tenant, final String clientId,
+            final String clientSecret, final String scope) {
+        this.accessToken = accessToken;
+        this.tenant = tenant;
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+        this.scope = scope;
+    }
+
+    @Override
+    public CompletableFuture<String> getAuthorizationTokenAsync(URL requestUrl) {
+        final String uri = UriComponentsBuilder
+                .fromHttpUrl("https://login.microsoftonline.com/" + tenant + "/oauth2/v2.0/token")
+                .queryParam("grant_type", "urn:ietf:params:oauth:grant-type:jwt-bearer")
+                .queryParam("client_id", clientId).queryParam("client_secret", clientSecret)
+                .queryParam("assertion", accessToken).queryParam("scope", scope)
+                .queryParam("requested_token_use", "on_behalf_of").build().toUriString();
+
+        return WebClient.create(uri).get().retrieve().bodyToMono(String.class).toFuture();
+    }
+
+}
--- src/main/java/com/matthewcasperson/onenote/controllers/GraphMeController.java
+++ src/main/java/com/matthewcasperson/onenote/controllers/GraphMeController.java
@@ -1,11 +1,23 @@
 package com.matthewcasperson.onenote.controllers;
 
+import com.microsoft.graph.requests.GraphServiceClient;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GraphMeController {
-    
+    @GetMapping("/me")
     public String getGraphMe() {
         return "me";
+    }
+
+    private GraphServiceClient buildGraphClient() {
+        final TokenCredentialAuthProvider tokenCredentialAuthProvider = new TokenCredentialAuthProvider(scopes, clientSecretCredential);
+        
+        return GraphServiceClient
+            .builder()
+            .authenticationProvider(tokenCredentialAuthProvider)
+            .buildClient();
     }
 }
