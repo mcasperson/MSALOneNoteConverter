3d4e78153dbca64f7570356865376e01f5420e59
Matthew Casperson <matthewcasperson@gmail.com>
2021-10-10 19:51:47 +1000

    Fixed AADAuthenticationProperties

--- src/main/java/com/matthewcasperson/onenote/controllers/GraphMeController.java
+++ src/main/java/com/matthewcasperson/onenote/controllers/GraphMeController.java
@@ -5,6 +5,8 @@ import com.matthewcasperson.onenote.authproviders.OboAuthenticationProvider;
 import com.microsoft.graph.requests.GraphServiceClient;
 import com.microsoft.graph.models.User;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,20 +17,26 @@ import org.springframework.security.oauth2.client.authentication.OAuth2Authentic
 
 @Controller
 public class GraphMeController {
+
+  @Autowired
+  AADAuthenticationProperties azureAd;
+
     @GetMapping("/me")
-    public ModelAndView getGraphMe(AADAuthenticationProperties azureAd) {
+    public ModelAndView getGraphMe() {
 
-        // JwtAuthenticationToken oauthToken = (JwtAuthenticationToken) SecurityContextHolder
-        //     .getContext()
-        //     .getAuthentication();
+         final OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) SecurityContextHolder
+             .getContext()
+             .getAuthentication();
 
-        // final GraphServiceClient client = buildGraphClient(
-        //         oauthToken.getToken().getTokenValue(), 
-        //         azureAd.getTenantId(),
-        //         azureAd.getClientId(), 
-        //         azureAd.getClientSecret(),
-        //         "https://graph.microsoft.com/user.read+offline_access");
-        // final User me = client.me().buildRequest().get();
+        final String token = ((OidcUser) oauthToken.getPrincipal()).getIdToken().getTokenValue();
+
+         final GraphServiceClient client = buildGraphClient(
+             token,
+             azureAd.getTenantId(),
+             azureAd.getClientId(),
+             azureAd.getClientSecret(),
+             "https://graph.microsoft.com/user.read+offline_access");
+         final User me = client.me().buildRequest().get();
         final ModelAndView mav = new ModelAndView("me");
         // mav.addObject("me", me);
         return mav;
