f8e803f967bba710f669771e759cb76ed2dfc158
Matthew Casperson <matthewcasperson@gmail.com>
2021-10-12 14:41:26 +1000

    Working on minimal frontend

--- src/main/java/com/matthewcasperson/onenote/controllers/GraphMeController.java
+++ src/main/java/com/matthewcasperson/onenote/controllers/GraphMeController.java
@@ -1,59 +1,34 @@
 package com.matthewcasperson.onenote.controllers;
 
-import com.azure.spring.autoconfigure.aad.AADAuthenticationProperties;
-import com.matthewcasperson.onenote.authproviders.OboAuthenticationProvider;
-import com.microsoft.graph.requests.GraphServiceClient;
-import com.microsoft.graph.models.User;
+import static org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
 
-import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-// import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.reactive.function.client.WebClient;
+import com.microsoft.graph.models.User;
 
 @Controller
 public class GraphMeController {
 
   @Autowired
-  AADAuthenticationProperties azureAd;
+  WebClient webClient;
 
   @GetMapping("/me")
-  public ModelAndView getGraphMe(@RegisteredOAuth2AuthorizedClient("api") OAuth2AuthorizedClient client) {
+  public String getGraphMe(@RegisteredOAuth2AuthorizedClient("api") OAuth2AuthorizedClient client) {
 
-    final GraphServiceClient graphClient = buildGraphClient(
-        client.getAccessToken().getTokenValue(),
-        azureAd.getTenantId(),
-        azureAd.getClientId(),
-        azureAd.getClientSecret(),
-        "https://graph.microsoft.com/user.read");
-    final User me = graphClient.me().buildRequest().get();
-    final ModelAndView mav = new ModelAndView("me");
-    return mav;
-  }
+    final User user = webClient
+        .get()
+        .uri("http://localhost:8081/me")
+        .attributes(oauth2AuthorizedClient(client))
+        .retrieve()
+        .bodyToMono(User.class)
+        .block();
 
-  private GraphServiceClient buildGraphClient(
-      final String accessToken,
-      final String tenantId,
-      final String clientId,
-      final String clientSecret,
-      final String scopes) {
-    final OboAuthenticationProvider oboAuthenticationProvider = new OboAuthenticationProvider(
-        accessToken,
-        tenantId,
-        clientId,
-        clientSecret,
-        scopes);
+    System.out.println(user.givenName + " " + user.surname);
 
-    return GraphServiceClient.builder()
-        .authenticationProvider(oboAuthenticationProvider)
-        .buildClient();
+    return "me";
   }
 }
