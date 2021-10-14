9cc248407f10f6564a6bddedb66b10ec55c2d39f
Matthew Casperson <matthewcasperson@gmail.com>
2021-10-15 09:49:01 +1000

    Showed markdown in html

--- src/main/java/com/matthewcasperson/onenote/controllers/OneNoteController.java
+++ src/main/java/com/matthewcasperson/onenote/controllers/OneNoteController.java
@@ -2,7 +2,6 @@ package com.matthewcasperson.onenote.controllers;
 
 import static org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
 
-import javax.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.reactive.function.client.WebClient;
 import com.microsoft.graph.models.User;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class OneNoteController {
@@ -31,11 +31,11 @@ public class OneNoteController {
 
     System.out.println(user.givenName + " " + user.surname);
 
-    return "me";
+    return "markdown";
   }
 
   @GetMapping("/notes/{name}/markdown")
-  public String getGraphMe(
+  public ModelAndView getGraphMe(
       @RegisteredOAuth2AuthorizedClient("api") final OAuth2AuthorizedClient client,
       @PathVariable("name") final String name) {
     final String markdown = webClient
@@ -46,8 +46,8 @@ public class OneNoteController {
         .bodyToMono(String.class)
         .block();
 
-    System.out.println(markdown);
-
-    return "me";
+    final ModelAndView mav = new ModelAndView("markdown");
+    mav.addObject("markdown", markdown);
+    return mav;
   }
 }
