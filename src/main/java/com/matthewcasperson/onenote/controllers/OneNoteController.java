44c864700a911387aafa5ff49ef8d7cc9c733fd2
Matthew Casperson <matthewcasperson@gmail.com>
2021-10-16 15:36:35 +1000

    Allowed onenote page to be previewed, and then downloaded as markdown.

--- src/main/java/com/matthewcasperson/onenote/controllers/OneNoteController.java
+++ src/main/java/com/matthewcasperson/onenote/controllers/OneNoteController.java
@@ -3,12 +3,18 @@ package com.matthewcasperson.onenote.controllers;
 import static org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
 
 import java.util.List;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.reactive.function.client.WebClient;
 import com.microsoft.graph.models.User;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,22 +24,6 @@ public class OneNoteController {
 
   @Autowired
   WebClient webClient;
-
-  @GetMapping("/me")
-  public String getGraphMe(@RegisteredOAuth2AuthorizedClient("api") final OAuth2AuthorizedClient client) {
-
-    final User user = webClient
-        .get()
-        .uri("http://localhost:8081/me")
-        .attributes(oauth2AuthorizedClient(client))
-        .retrieve()
-        .bodyToMono(User.class)
-        .block();
-
-    System.out.println(user.givenName + " " + user.surname);
-
-    return "markdown";
-  }
 
   @GetMapping("/")
   public ModelAndView getIndex(
@@ -51,8 +41,33 @@ public class OneNoteController {
     return mav;
   }
 
+  @GetMapping("/notes/{name}")
+  public ModelAndView getPageView(@PathVariable("name") final String name) {
+    final ModelAndView mav = new ModelAndView("pageview");
+    mav.addObject("iframesrc", "/notes/" + name + "/html");
+    mav.addObject("markdownsrc", "/notes/" + name + "/markdown");
+    return mav;
+  }
+
+  @GetMapping(value = "/notes/{name}/html", produces = MediaType.TEXT_HTML_VALUE)
+  @ResponseBody
+  public String getNoteHtml(
+      @RegisteredOAuth2AuthorizedClient("api") final OAuth2AuthorizedClient client,
+      @PathVariable("name") final String name,
+      final HttpServletResponse response) {
+    response.setHeader("X-Frame-Options", "SAMEORIGIN");
+    response.setHeader("Content-Security-Policy", " frame-ancestors 'self'");
+    return webClient
+        .get()
+        .uri("http://localhost:8081/notes/" + name + "/html")
+        .attributes(oauth2AuthorizedClient(client))
+        .retrieve()
+        .bodyToMono(String.class)
+        .block();
+  }
+
   @GetMapping("/notes/{name}/markdown")
-  public ModelAndView getGraphMe(
+  public ResponseEntity<byte[]> getNoteMarkdown(
       @RegisteredOAuth2AuthorizedClient("api") final OAuth2AuthorizedClient client,
       @PathVariable("name") final String name) {
     final String markdown = webClient
@@ -63,8 +78,11 @@ public class OneNoteController {
         .bodyToMono(String.class)
         .block();
 
-    final ModelAndView mav = new ModelAndView("markdown");
-    mav.addObject("markdown", markdown);
-    return mav;
+    final HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.TEXT_MARKDOWN);
+    final String filename = "page.md";
+    headers.setContentDispositionFormData(filename, filename);
+    headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+    return new ResponseEntity<>(markdown.getBytes(), headers, HttpStatus.OK);
   }
 }
